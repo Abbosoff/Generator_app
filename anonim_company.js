bc956152986bcc6ee72ab9534bfafa56312e0c01
Abbosoff <90924880+Abbosoff@users.noreply.github.com>
2021-09-17 21:37:32 +0500

    Add files via upload

---
+++ anonim_company.js
@@ -0,0 +1,23 @@
+// let d=10000
+// console.log(xx2)
+// var r=toString(xx2)
+let users=[]
+
+    
+for(let i = 0; i < 1000; i++) {
+    let xx2 = parseInt(1000000 + Math.random() * 100000000)
+    let aa=parseInt(0+Math.random()*2)
+    users.push(`+998(9${aa})${xx2}`)
+
+    
+}
+console.log(users)
+
+
+
+
+// users.push(xx)
+// for(let i of x){
+//     users.push(i)
+// }
+console.log(users)
